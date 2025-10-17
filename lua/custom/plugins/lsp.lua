3c48bab38e45e0fc51719b411a3aa24062cdcf5b
Bjørn Kristian Strand <bjkristian@proton.me>
2025-10-17 23:54:24 +0200

    chore(lsp): update lsp config

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,82 +1,78 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-      -- Autoformatting
-      "stevearc/conform.nvim",
+			-- Autoformatting
+			"stevearc/conform.nvim",
 
-      -- Schema Information
-      "b0o/SchemaStore.nvim",
-    },
-    config = function()
-      require("mason").setup()
+			-- Schema Information
+			"b0o/SchemaStore.nvim",
+		},
+		config = function()
+			require("mason").setup()
 
-      -- LSP's --
-      require("mason-lspconfig").setup({
-        automatic_installation = false,
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "jsonls",
-          "yamlls",
-          "ansiblels",
-        },
-      })
+			-- LSP's --
+			require("mason-lspconfig").setup({
+				automatic_installation = false,
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"jsonls",
+					"yamlls",
+					"ansiblels",
+				},
+			})
 
-      -- Tools --
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "stylua",
-        },
-      })
+			-- Tools --
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+				},
+			})
 
-      require("lspconfig").lua_ls.setup({})
+			vim.lsp.config("lua_ls", {})
+			vim.lsp.config("gopls", {})
+			vim.lsp.config("ansiblels", {})
+			vim.lsp.config("jsonls", {
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			})
 
-      require("lspconfig").gopls.setup({})
+			vim.lsp.config("yamlls", {
+				settings = {
+					yaml = {
+						format = {
+							enable = true,
+							singleQuote = true,
+						},
+						schemas = require("schemastore").yaml.schemas(),
+					},
+				},
+			})
 
-      require("lspconfig").jsonls.setup({
-        settings = {
-          json = {
-            schemas = require("schemastore").json.schemas(),
-          },
-        },
-      })
+			-- Autoformatting --
+			require("custom.autoformat").setup()
 
-      require("lspconfig").yamlls.setup({
-        settings = {
-          yaml = {
-            format = {
-              enable = true,
-              singleQuote = true,
-            },
-            schemas = require("schemastore").yaml.schemas(),
-          },
-        },
-      })
-
-      require("lspconfig").ansiblels.setup({})
-
-      -- require("lspconfig").bicep.setup({})
-
-      -- Autoformatting --
-      require("custom.autoformat").setup()
-
-      vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
-    end,
-  },
+			vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
+		end,
+	},
 }
