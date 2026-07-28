2b916af2bdbd0ec53b3445ea4cfbbfcccdb1545b
Bjørn Kristian Strand <bjkristian@proton.me>
2026-07-28 11:31:49 +0200

    add rendermarkdown and rewrite treesitter

--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -2,43 +2,45 @@ local M = {}
 
 M.setup = function()
   require("nvim-treesitter").setup({
-    ensure_install = {
-      "bash",
-      "vim",
-      "vimdoc",
-      "bash",
-      "go",
-      "gomod",
-      "gowork",
-      "gosum",
-      "cue",
-      "python",
-      "toml",
-      "sql",
-      "yaml",
-      "json",
-      "lua",
-      "dockerfile",
-      "gitignore",
-      "git_config",
-      "gitcommit",
-      "hcl",
-      "html",
-      "markdown",
-      "markdown_inline",
-      "regex",
-      "requirements",
-      "sql",
-      "toml",
-      "csv",
-      "diff",
-    },
-    auto_install = true,
-    sync_install = false,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = true,
-    },
+    install_dir = vim.fn.stdpath("data") .. "/site",
+  })
+
+  local parsers = {
+    "bash",
+    "vim",
+    "vimdoc",
+    "go",
+    "gomod",
+    "gowork",
+    "gosum",
+    "cue",
+    "python",
+    "toml",
+    "sql",
+    "yaml",
+    "json",
+    "lua",
+    "dockerfile",
+    "gitignore",
+    "git_config",
+    "gitcommit",
+    "hcl",
+    "html",
+    "markdown",
+    "markdown_inline",
+    "regex",
+    "requirements",
+    "csv",
+    "diff",
+  }
+
+  require("nvim-treesitter").install(parsers)
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = parsers,
+    callback = function()
+      vim.treesitter.start()
+    end,
   })
 end
 
